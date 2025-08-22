523fdeb2f2c08ea4c7be8aafb7a728c8f6d2924f
yami-beta <yami-beta@users.noreply.github.com>
2025-08-22 14:44:20 +0900

    feat!: reset project

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: cloudflare()
-});
